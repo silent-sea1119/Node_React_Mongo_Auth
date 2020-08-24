32203cf8b053d2f2c012f7918ae824ac7b3dfdf8
silent-sea1119 <sandragonzales56t@gmail.com>
2020-08-24 12:22:20 +0200

    Frontend - Added Signup Component

--- frontend/app/src/components/auth/Signup/Signup.js
+++ frontend/app/src/components/auth/Signup/Signup.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import axios from 'axios';
+import * as EmailValidator from "email-validator";
 
 
 
@@ -9,16 +10,36 @@ const Signup = (props) => {
     function handleSignupSubmit(e) {
         // stop submit
         e.preventDefault();
+
+        // get values from input
+        const inputs = Array.from(e.target.children)
+        const email = inputs.find(node => node.name === "email").value
+        const password = inputs.find(node => node.name === "password").value
+        const passwordConfirm = inputs.find(node => node.name === "passwordConfirm").value
+
+        // validate email
+        if (!EmailValidator.validate(email)) return console.log('wrong email');
+        // validate password
+        if (typeof password !== 'string' || password.length < 4) return console.log('password to short')
+        if (password !== passwordConfirm) return console.log("Type the same password twice.")
+
+        // setup data and headers for axios call
         const url = 'http://localhost:4000/signup';
         const headers = {
             'Content-Type': 'application/json'
         }
+        const data = {
+            "email": email,
+            "password": password
+        }
 
-        const inputs = Array.from(e.target.children)
-        const email = inputs.find(node => node.name === "email").value
-        const password = inputs.find(node => node.name === "password").value
-        console.log(email, password)
-
+        // call api
+        axios.post(url, data, headers)
+            .then(res => {
+                console.log(res.data)
+                console.log(res.status)
+            })
+            .catch(err => console.log(err))
     };
 
     return (
@@ -27,6 +48,7 @@ const Signup = (props) => {
             <form onSubmit={handleSignupSubmit}>
                 <input type="text" name="email" id="emailInput" />
                 <input type="password" name="password" id="password" />
+                <input type="password" name="passwordConfirm" id="passwordConfirm" />
                 <input type="submit" id="signupSubmitBtn"
                     className="submitBtn" value="Signup" />
             </form>
